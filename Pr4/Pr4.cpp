9e8d8944218c37cdde745a15ef6fc1fde42b3f68
Yaros <prokip.yarik47@gmail.com>
2023-11-18 15:28:45 +0200

    Prac5

--- Pr4/Pr4.cpp
+++ Pr4/Pr4.cpp
@@ -1,104 +1,85 @@
 #include <iostream>
+
 using namespace std;
 
-class Car {
+class Dish {
 public:
-    virtual string getModel() = 0;
-    virtual int getHorsepower() = 0;
-    virtual int getYear() = 0;
-    virtual int getCost() = 0;
-    virtual ~Car() {};
+    virtual void prepareIngredients() = 0;
+    virtual void cook() = 0;
+    virtual void serve() = 0;
 };
 
-class Nissan : public Car {
+class Pizza : public Dish {
 public:
-    Nissan(int _horsepower, int _year) {
-        horsepower = _horsepower;
-        year = _year;
+    void prepareIngredients() override {
+        std::cout << "Preparation of ingredients for pizza:" << std::endl;
+        std::cout << "-dough" << std::endl;
+        std::cout << "-sauce" << std::endl;
+        std::cout << "-pizza topping" << std::endl;
     }
-    string getModel() {
-        return model;
+
+    void cook() override {
+        std::cout << "Cooking pizza in the oven" << std::endl;
     }
-    int getHorsepower() {
-        return horsepower;
+
+    void serve() override {
+        std::cout << "Serving pizza to the table" << std::endl;
     }
-    int getYear() {
-        return year;
-    }
-    int getCost() {
-        return cost;
-    }
-private:
-    string model = "Nissan";
-    int cost = 15000;
-    int horsepower;
-    int year;
 };
 
-class Toyota : public Car {
+class Soup : public Dish {
 public:
-    Toyota(int _horsepower, int _year) {
-        horsepower = _horsepower;
-        year = _year;
+    void prepareIngredients() override {
+        std::cout << "Preparation of ingredients for soup:" << std::endl;
+        std::cout << "-bouillon" << std::endl;
+        std::cout << "-vegetables" << std::endl;
+        std::cout << "-meat" << std::endl;
     }
-    string getModel() {
-        return model;
+
+    void cook() override {
+        std::cout << "Cooking soup on the stove" << std::endl;
     }
-    int getHorsepower() {
-        return horsepower;
+
+    void serve() override {
+        std::cout << "Serve the soup in a plate" << std::endl;
     }
-    int getYear() {
-        return year;
-    }
-    int getCost() {
-        return cost;
-    }
-private:
-    string model = "Supra";
-    int cost = 12000;
-    int horsepower;
-    int year;
 };
 
-class BMW : public Car {
+class Builder {
 public:
-    BMW(int _horsepower, int _year) {
-        horsepower = _horsepower;
-        year = _year;
-    }
-    string getModel() {
-        return model;
-    }
-    int getHorsepower() {
-        return horsepower;
-    }
-    int getYear() {
-        return year;
-    }
-    int getCost() {
-        return cost;
-    }
-private:
-    string model = "E36";
-    int cost = 8000;
-    int horsepower;
-    int year;
+    virtual void build(Dish* dish) = 0;
 };
 
-int main()
-{
-    Car* cars[] = { new Nissan(1020,2007), new Toyota(730, 2015), new BMW(715,2000) };
-    for (int i = 0; i < size(cars); i++) {
-        cout << "Model: " << cars[i]->getModel() << endl
-            << "Horsepower: " << cars[i]->getHorsepower() << endl
-            << "Year: " << cars[i]->getYear() << endl
-            << "Cost: " << cars[i]->getCost() << endl
-            << "--------------" << endl;
+class PizzaBuilder : public Builder {
+public:
+    void build(Dish* dish) override {
+        dish->prepareIngredients();
+        dish->cook();
+        dish->serve();
     }
+};
 
-    for (int i = 0; i < size(cars); i++) {
-        delete cars[i];
+class SoupBuilder : public Builder {
+public:
+    void build(Dish* dish) override {
+        dish->prepareIngredients();
+        dish->cook();
+        dish->serve();
     }
+};
+
+int main() {
+    Dish* dish;
+
+    // ЅудуЇмо п≥цу
+    PizzaBuilder pizzaBuilder;
+    dish = new Pizza();
+    pizzaBuilder.build(dish);
+
+    // ЅудуЇмо суп
+    SoupBuilder soupBuilder;
+    dish = new Soup();
+    soupBuilder.build(dish);
 
     return 0;
 }
